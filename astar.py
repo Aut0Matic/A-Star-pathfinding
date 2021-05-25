4407ed7907ae66e45fbec38679f480f9dffa7cae
Aut0Matic <aalindsay@me.com>
2021-05-25 21:53:06 +0930

    Initialising Classes

--- astar.py
+++ astar.py
@@ -2,7 +2,66 @@ import pygame
 import math
 from queue import PriorityQueue
 
-width = 800
-win = pygame.display.set_mode((width, width))
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* pathfinding algorithm")
 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE= (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+class square:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE # Setting the initial color to white.
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == PURPLE
+
+    def reset(self):
+        self.color == WHITE
+
+    def make_closed(self):
+        self.color == RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (800, 800))
